c8f240203de52e420de7e6cd83f846a0e79bf3da
M.BasilKhan <basilkhan1250@gmail.com>
2025-01-09 21:28:20 +0500

    updated

--- src/gettingdata.jsx
+++ src/gettingdata.jsx
@@ -1,2 +1,9 @@
 import React from "react";
 
+async function gettingmessages() {
+    const apicalling = await fetch(`https://www.personalityforge.com/api/chat/index.php?apiKey=TtDcXGyNzimVDe0R&chatBotID=63906&message=how%27s+your+day+going%3F&externalID=abc-6391845436878`);
+    const data = await apicalling.json();
+    console.log(data);
+}
+
+gettingmessages()
